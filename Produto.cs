7d83c24e7360505e2fd3ad7e2ad48e83b69cbf3b
gothwavy <ryanalmeidadev@gmail.com>
2024-12-22 17:59:34 -0300

    Finalizado✅

---
+++ Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Acesso
+{
+    internal class Produto
+    {
+        /*
+        public    -> Atributos e metodos visiveis em qualquer classe
+        private   -> Atributos e metodos visiveis apenas na classe onde sao criados
+        protected -> Atributos e metodos visiveis em classes onde sao criados ou herdados
+        */
+
+        public string nome;
+        private double valor;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _05_Acesso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+        }
+    }
+}
